5da94c9a874b612393a57de35df75e4dd25b9fb3
etentlabs <19627177+OkayJosh@users.noreply.github.com>
2019-04-03 08:39:22 +0100

    Modified the form.py file  to be verbose and to include the other models

--- Project/Project/myproject/forms.py
+++ Project/Project/myproject/forms.py
@@ -1,8 +1,12 @@
 from django.forms import ModelForm
-from .models import Project
+from .models import ( Project, SubProject, SubProjectAppriasal,
+                        SubProjectCloseout, ProjectFund)
 
 
 class ProjectForm(ModelForm):
+    """
+    The Project Form
+    """
     class Meta:
         model = Project
         fields = ['code_num', 'id_num', 'pname',
@@ -10,3 +14,34 @@ class ProjectForm(ModelForm):
         exclude = ['created_by',]
 
         field_classes = "pink darken-1"
+
+class SubProjectForm(ModelForm):
+    """
+    The Subproject Form
+    """
+    class Meta:
+        model = SubProject
+        exclude =['owned_by']
+
+class SubProjectAppriasalForm(ModelForm):
+    """
+    The SubProject Appraisal Form
+    """
+
+    class Meta:
+        model = SubProjectAppriasal
+        exclude =['owned_by']
+
+class SubProjectCloseoutForm (ModelForm):
+    """
+    The SubProject close out Form
+
+    """
+    class Meta:
+        model = SubProjectCloseout
+        exclude = ['owned_by']
+
+class ProjectFundForm(ModelForm):
+    class Meta:
+        model = ProjectFund
+        exclude =['owned_by']
