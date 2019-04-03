243a949278a23c5aad1ad7c9187d4d68194d5a41
etentlabs <19627177+OkayJosh@users.noreply.github.com>
2019-04-03 08:30:05 +0100

    Modified the project's model to be verbose

--- Project/Project/myproject/models.py
+++ Project/Project/myproject/models.py
@@ -4,20 +4,25 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Project(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    code_num = models.IntegerField()
-    id_num = models.IntegerField()
-    pname = models.CharField(max_length=300)
-    _class = models.CharField(max_length=300)
-    des = models.TextField()
-    owner = models.CharField(max_length=300)
+    code_num = models.IntegerField()#--------------->                                           Project Code number FGN or STA
+    id_num = models.IntegerField()  #--------->                                                 Project main identification Number
+    pname = models.CharField(max_length=300)# --------------->                                  Project name
+    _class = models.CharField(max_length=300) #----------------->                               Classification of project
+    des = models.TextField()                   #------------->                                  Description of project
+    owner = models.CharField(max_length=300) #----------------------->                          Owner of the projec: the agency
 
     def __str__(self):
         return self.pname
 
 class SubProject(models.Model):
+    """
+    This models the Project Registration info
+
+    """
     owned_by = models.ForeignKey(Project, on_delete=models.CASCADE)
     spname = models.CharField(max_length=300, default="no name") #------------> sub project name
     scode_num = models.IntegerField() #-------------->                          Sub project code number
+    id_num_beneficiary = models.CharField(max_length=300, default="no id")#     Idenfication number of beneficiary
     sid_num = models.IntegerField() #-------------->                            Sub project identification number
     cost = models.IntegerField()
     benefit = models.TextField() #-------------->                               The benefit of the project
@@ -28,6 +33,10 @@ class SubProject(models.Model):
     leader = models.CharField(max_length=300) #----------->                     The leader of the subproject
 
 class SubProjectAppriasal (models.Model):
+    """
+    This models the supproject appriasal info
+
+    """
     owned_by = models.OneToOneField(SubProject, on_delete=models.CASCADE) 
     ap_date = models.DateField()    #---->                                      appriases date
     delay_reason = models.TextField()  
@@ -35,6 +44,10 @@ class SubProjectAppriasal (models.Model):
     a_name = models.CharField(max_length=300) #                                 appriaser's name
 
 class SubProjectCloseout (models.Model):
+    """
+    This models the supproject closeout info
+
+    """
     owned_by = models.OneToOneField(SubProject, on_delete=models.CASCADE) 
     id_num = models.IntegerField()
     sid_num = models.IntegerField() #---->                                      subproject identification number
@@ -43,5 +56,17 @@ class SubProjectCloseout (models.Model):
     remark = models.TextField() #
     a_name = models.CharField(max_length=300) #                                 appriaser's name
 
+class ProjectFund(models.Model):
+    """
+    This models who will fund the project
+
+    """
+    owned_by = models.OneToOneField(Project, on_delete=models.CASCADE)
+    name_funder = models.CharField(max_length=300) #--------->                   Name of the funding agency
+    nature_funding = models.TextField()# ------------------->                    The nature of the funding
+    amount = models.IntegerField()# ---------------------->                      Amount founded
+    currency = models.CharField()# --------------------->                        Currency of the funded amount
+
+    
 
 
